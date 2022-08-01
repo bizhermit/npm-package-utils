130a89fe8017a12319b9c4da28fff88ddcbdb141
bizhermit <rsenda@bizhermit.com>
2022-08-01 22:41:56 +0900

    brushup

--- README.md
+++ README.md
@@ -9,9 +9,30 @@ npm i -D @bizhermit/npm-package-utils
 **Usage**  
 
 generate or update `src/package.json` from `./package.json`
+
 ```bash
 npx npm-package-utils init
 ```
+
+---
+
+generate index declaretion file.  
+target extension is `.ts` and `.tsx`.  
+
+```bash
+npx npm-package-utils d.ts <options>
+```
+
+* `--ignore` ignore file or dir name.  `cli.ts` and `node_modules`, start width `.` directories are absolutely ignored.
+* `--outDir` out dir name. default: `./src`
+* `--outName` out file name. default: `index.d.ts`
+* `--quiet` no console.
+
+```bash
+npx npm-package-utils d.ts --quiet --ignore hgoe.ts,fuga.ts --outDir ../package --outName custom.d.ts
+```
+
+---
 
 copy files from `./src` into `./package`  
 - `package.json`
--- src/dist/generate-declaretion.ts
+++ src/dist/generate-declaretion.ts
@@ -24,6 +24,7 @@ const generateDeclaretionFile = (cwd: string, options?: {
     const files = readdirSync(path.join(cwd, "src", relativePath));
     files.forEach(file => {
       if (ignoreFiles.indexOf(file) >= 0) return;
+      if (file.startsWith(".")) return;
       const fileStat = statSync(path.join(cwd, "src", relativePath, file));
       if (fileStat.isDirectory()) {
         impl(path.join(relativePath, file));
@@ -42,14 +43,16 @@ const generateDeclaretionFile = (cwd: string, options?: {
       }
       if (contents.length > 0) contents += "\n";
       contents += `declare module "${baseName}/${relativePathName}" {\n  const ${defaultExportName}: typeof import("./${relativePathName}");\n  // tslint:disable-next-line:export-just-namespace\n  export = ${defaultExportName};\n}`;
-      if (writeLog) process.stdout.write(`- ${file} >> ${defaultExportName}\n`);
+      if (writeLog) process.stdout.write(`- ${relativePathName} >> ${defaultExportName}\n`);
     });
   };
   impl("");
   if (!existsSync(outDir)) {
     mkdirSync(outDir, { recursive: true });
   }
-  writeFileSync(path.join(outDir, options?.outName || "index.d.ts"), contents);
+  const outFileName = path.join(outDir, options?.outName || "index.d.ts");
+  writeFileSync(outFileName, contents);
+  if (writeLog) process.stdout.write(`out: ${outFileName}\n`);
 };
 
 export default generateDeclaretionFile;
