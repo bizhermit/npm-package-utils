b86ee08784ca399008ca3b8f5ea16f780bc2b17d
bizhermit <rsenda@bizhermit.com>
2022-08-01 23:33:45 +0900

    bug fix

--- src/dist/pack.ts
+++ src/dist/pack.ts
@@ -29,7 +29,7 @@ const packNpmPackage = (cwd: string) => {
         if ((npmPkg.files ?? []).length > 0 && npmPkg.files.indexOf("index.d.ts") < 0) {
           npmPkg.files.push("index.d.ts");
         }
-      } else if (path.join(cwd, "package/dist/index.d.ts")) {
+      } else if (existsSync(path.join(cwd, "package/dist/index.d.ts"))) {
         npmPkg.types = "dist/index.d.ts";
         if ((npmPkg.files ?? []).length > 0 && npmPkg.files.indexOf("dist") < 0) {
           npmPkg.files.push("dist");
